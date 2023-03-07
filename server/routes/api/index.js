70378a5ba3eeb334683cc20cec658f642f648001
naser421 <naser421@gmail.com>
2023-03-07 20:26:51 +0300

    Code revamp

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -6,3 +6,9 @@ router.use('/users', userRouter);
 router.use('/question', questionRouter);
 
 module.exports = router;
+
+const path = require("path"); // include this in the file
+
+router.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
