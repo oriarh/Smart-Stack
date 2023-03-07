4dd3f7e834f03d393c5225616e21d1445dcbd976
SeanBristol-Lee <seanbristol81@gmail.com>
2023-03-07 03:53:14 -0800

    set up sign in

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
       <div className="">
         <header className="">
           <FlashcardApp/>
-          {/* <Signup/> */}
+          <Signup/>
           {/* <Routes>
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/login' element={<Login/>}/>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,6 +2,9 @@ import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, )
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            
+        }
     }
 `;
