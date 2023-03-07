7409a35f505eb5f9db0ebcddb69cdf17a429594c
Naser Suleiman <nasersuleiman421@gmail.com>
2023-03-06 18:46:36 -0800

    question seeds

--- server/seeds/question-seeds.js
+++ server/seeds/question-seeds.js
@@ -1,1 +1,16 @@
-const { Question } = require
+const Question = require ('../models/product');
+
+const mongoose = require ('mongoose');
+
+const connection = require ('../config/connection');
+
+const questions = [
+    new Question ({
+    questionTitle: "Binary Tree",
+    difficulty: "Hard",
+    questionText: "What is the max depth of a binary tree?",
+    answerText: "The maximum depth of a binary tree is the number of nodes along the longest path from the root node down to the farthest leaf node",
+    
+
+    })
+]
