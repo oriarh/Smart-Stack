86146b1a30c710efe7ec52140f7b74a590bdd5dd
Naser Suleiman <nasersuleiman421@gmail.com>
2023-03-06 19:12:47 -0800

    seeds

--- server/seeds/question-seeds.js
+++ server/seeds/question-seeds.js
@@ -10,27 +10,27 @@ const questions = [
     difficulty: "Hard",
     questionText: "What is the max depth of a binary tree?",
     answerText: "The maximum depth of a binary tree is the number of nodes along the longest path from the root node down to the farthest leaf node",
- })
+ }), 
 ]
 
 //connect mongoose
 mongoose
-.connect(String(dev.db), { useNewUrlParser: true })
-.catch(err => {
-  console.log(err.stack);
-  process.exit(1);
-})
-.then(() => {
-  console.log("connected to db in connection environment");
-});
+    .connect(String(dev.db), { useNewUrlParser: true })
+    .catch(err => {
+        console.log(err.stack);
+        process.exit(1);
+    })
+    .then(() => {
+        console.log("connected to db in connection environment");
+    });
 //save data. this is an async operation
 
 //after you make sure you seeded all the products, disconnect automatically
 questions.map(async (p, index) => {
-await p.save((err, result) => {
-  if (index === questions.length - 1) {
-    console.log("DONE!");
-    mongoose.disconnect();
-  }
-});
+    await p.save((err, result) => {
+        if (index === questions.length - 1) {
+            console.log("DONE!");
+            mongoose.disconnect();
+        }
+    });
 });
