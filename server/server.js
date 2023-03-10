0bbcdd27f768f54972fbe9428ec746edea9cfc78
SeanBristol-Lee <seanbristol81@gmail.com>
2023-03-09 20:59:36 -0800

    work

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname,"..", "client/build")));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"..", 'client/build/index.html'));
+    res.sendFile(path.join(__dirname,"..", "client/build/"));
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
