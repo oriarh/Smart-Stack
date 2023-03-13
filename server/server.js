69fe13e714d32c53cc52c07c24cd421c50b19402
SeanBristol-Lee <seanbristol81@gmail.com>
2023-03-13 02:01:20 -0700

    add to package.json

--- server/server.js
+++ server/server.js
@@ -18,13 +18,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname,"..", "client/build")));
 
-app.use(express.static(path.resolve(__dirname,"..", "client/build")));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"..", "client/build"));
-});
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+};
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
