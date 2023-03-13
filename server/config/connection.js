16e901c2c10d30e1c9859a46294e14eb3394f22e
SeanBristol-Lee <seanbristol81@gmail.com>
2023-03-12 17:04:44 -0700

    add to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const uri = "mongodb+srv://SeanBristolLee:Green777@yourClusterName.n9z04.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/smartStack_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
