5707077b47ca650919db8f065f7d195a49621811
SeanBristol-Lee <seanbristol81@gmail.com>
2023-03-13 02:15:08 -0700

    slight change to server

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-    app.use(express.static(path.resolve(__dirname,"..", "client/build")));
+app.use(express.static(path.resolve(__dirname,"..", "client/build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"..", "client/build"));
+});
 
 
 
